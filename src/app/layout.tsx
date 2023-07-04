7a94ca469774f143ae3900c6196dc8664203094c
SJ Homer <dev@sjhomer.com>
2023-07-03 23:02:20 -0400

    v16

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,22 @@
 import "./globals.css"
 import "highlight.js/styles/vs2015.css"
 import {Inter} from "next/font/google"
+import {Metadata} from "next"
 
 const inter = Inter({subsets: ["latin"]})
 
-export const metadata = {
-  title: "Recovery GTP",
-  description: "Restore your exported GTP conversations privately, and in style!",
+export const metadata: Metadata = {
+  title: "RecoveryGPT",
+  description: "Restore your exported GPT conversations privately, and in style!",
+  applicationName: "RecoveryGPT",
+  keywords: "ChatGPT, OpenAI, GPT, Conversations, Review, Privacy, User-Friendly, Secure, Offline, Code Snippets",
+  authors: {
+    name: "SJ Homer",
+    url: "https://github.com/sjhomer/recovery-gpt",
+  },
+  creator: "SJ Homer",
+  colorScheme: "dark",
+  icons: "/ringbouy.svg",
 }
 
 export default function RootLayout({
